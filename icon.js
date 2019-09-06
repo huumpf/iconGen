dcf925422d940ffcc2ca8cd529ed5b5ea771537a
huumpf <nico@windwalk.de>
2019-09-06 16:22:32 +0200

    Icons working

--- icon.js
+++ icon.js
@@ -1,52 +1,73 @@
 class Icon {
   
   constructor() {
-    this.radius = CFG.size / 2;
-    this.center = createVector( CFG.size / 2, CFG.size / 2 );
     this.points = this.makePoints();
-    this.vertices = this.makeVertices();
+    this.connections = this.makeConnections();
   }
 
   makePoints () {
-    let points = [];
-    let angle = 0;
-    for ( let i = 0; i < CFG.pointCount; i++) {
-      let pt = new p5.Vector.fromAngle( angle );
-      pt = pt.setMag( this.radius );
-      pt.add(this.center);
-      points.push( pt );
-      angle += TWO_PI / CFG.pointCount;
+    // Left and right side
+    let pt_count = Math.floor( random( CFG.min_pt_count, CFG.max_pt_count ) );
+    let points_left = [];
+    let points_right = [];
+    let points_center = [];
+    for (let i = 0; i < pt_count; i++) {
+      let pt_x = random( 0, CFG.icon_width * (CFG.center_offset / 2) );
+      let pt_y = random( 0, CFG.icon_height );
+      let pt_left = createVector( pt_x, pt_y );
+      let pt_right = createVector( CFG.icon_width - pt_x , pt_y );
+      points_left.push( pt_left );
+      points_right.push( pt_right );
     }
-    // console.log( points );
-    return points;
+    // Center axis
+    let y_inc = CFG.icon_height / CFG.axis_pt_count;
+    for (let i = 0; i < CFG.axis_pt_count; i++) {
+      let pt = createVector( CFG.icon_width / 2, i * y_inc );
+      points_center.push( pt );
+    }
+
+    return { left: points_left, right: points_right, center: points_center };
   }
 
-  makeVertices() {
-    let vertices = [];
-    for (let i = 0; i < 7; i++) {
-      let rand_i = Math.floor(random( 0, this.points.length ));
-      vertices.push( this.points[rand_i] );
+  makeConnections() {
+    let connections = [];
+    for (let i = 0; i < this.points.left.length; i++) {
+      var toCenter = Math.random() >= 0.5;
+      if (toCenter) {
+        let rand_i = floor( random( 0, this.points.center.length ) );
+        connections.push( [ this.points.left[i], this.points.center[rand_i] ] );
+        connections.push( [ this.points.right[i], this.points.center[rand_i] ] );
+      } else {
+        let rand_i = floor( random( 0, this.points.left.length ) );
+        if ( rand_i == i ) rand_i = floor( random( 0, this.points.left.length ) );
+        connections.push( [ this.points.left[i], this.points.right[rand_i] ] );
+        connections.push( [ this.points.right[i], this.points.left[rand_i] ] );
+      }
     }
-    return vertices;
+    return connections;
+  }
+
+  drawConnections() {
+    strokeWeight( 1 );
+    stroke( 255 );
+    for (let i = 0; i < this.connections.length; i++) {
+      line( this.connections[i][0].x, this.connections[i][0].y, this.connections[i][1].x, this.connections[i][1].y );
+    }
   }
 
   drawPoints() {
-    for ( let pt of this.points ) {
-      noFill();
-      strokeWeight( 1 );
-      stroke( 255 );
+    noFill();
+    strokeWeight( 2 );
+    stroke( 255 );
+    for ( let pt of this.points.left ) {
       point( pt.x, pt.y );
     }
-  }
-
-  drawShape() {
-    noStroke();
-    fill( 255 );
-    beginShape();
-    for ( let vert of this.vertices ) {
-      vertex( vert.x, vert.y );
+    for ( let pt of this.points.right ) {
+      point( pt.x, pt.y );
     }
-    endShape( CLOSE );
+    for ( let pt of this.points.center ) {
+      point( pt.x, pt.y );
+    }
   }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,25 +1,46 @@
 var canvas;
 
 const CFG = {
-  size : 30,
-  count : 25,
-  pointCount : 20
+  icon_width : 20,
+  icon_height: 25,
+  min_pt_count: 2,
+  max_pt_count: 4,
+  axis_pt_count: 4,
+  center_offset: 0.66,
+
+  // For drawing multiple icons
+  icon_margin: 50,
+  l_h: 6
 }
 
-let icon;
+let icons = [];
 
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight);
   noLoop();
 
-  icon = new Icon();
+  for (let i = 0; i < Math.pow(CFG.l_h, 2); i++) {
+    icons.push( new Icon() );
+  }
+
 }
 
 function draw() {
-  translate( width/2, height/2 );
+
   background( 40 );
-  strokeWeight( 1 );
-  stroke( 255 );
-  // icon.drawPoints();
-  icon.drawShape();
+  
+  let total_width = CFG.l_h * CFG.icon_width + (CFG.l_h - 1) * CFG.icon_margin;
+  let total_height = CFG.l_h * CFG.icon_height + (CFG.l_h - 1) * CFG.icon_margin;
+  translate( width/2 - total_width/2, height/2 - total_height/2 );
+  
+  for (let i = 0; i < icons.length; i++) {
+    icons[i].drawPoints();
+    icons[i].drawConnections();
+    if ((i + 1) % CFG.l_h == 0 && i > 0) {
+      translate( -total_width + CFG.icon_width, CFG.icon_height + CFG.icon_margin );
+    } else {
+      translate( CFG.icon_width + CFG.icon_margin, 0 );
+    }
+  }
+
 }
